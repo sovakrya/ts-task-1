ce6872403928b3c98fe0c49e1fc0afe872e1ce22
sovaKrya <tyan_34ee@mail.ru>
2024-09-17 15:28:52 +0300

    added typing in solution.ts

---
+++ solution.ts
@@ -0,0 +1,39 @@
+enum ACTION {
+  ADD = "ADD",
+  DELETE = "DELETE",
+  UPDATE = "UPDATE",
+}
+
+type Person = {
+  id: number,
+  name: string,
+  items: Person[] | [] |  null
+}
+
+
+// TODO Enum
+const actionMap = [ACTION.ADD, ACTION.DELETE, ACTION.UPDATE];
+
+// TODO Interface
+function func(id: number, isActive: boolean, callback: Function): (string | number)[][] {
+  return [["name", 123]];
+}
+func(1, true, () => {});
+
+// TODO Interface
+const a: Person = {
+  id: 123,
+  name: "Eric",
+  items: [
+    {
+      id: 55,
+      name: "Masha",
+      items: [],
+    },
+    {
+      id: 34,
+      name: "Max",
+      items: null,
+    },
+  ],
+};
